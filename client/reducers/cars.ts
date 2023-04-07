c46dbe8f8d61f57f010b4bce5ab01db2f0d14783
Raghav Sidharth <121782551+Raghav-Sidharth@users.noreply.github.com>
2023-04-07 13:44:04 +1200

    Completed reducer

---
+++ client/reducers/cars.ts
@@ -0,0 +1,47 @@
+import { Cars } from '../../models/Cars'
+import {
+  CarsAction,
+  FETCH_CARS_FULFILLED,
+  FETCH_CARS_PENDING,
+  FETCH_CARS_REJECTED,
+} from '../actions/carsAction'
+
+interface CarsState {
+  data: Cars[] | undefined
+  error: string | undefined
+  loading: boolean
+}
+
+const initialState: CarsState = {
+  data: undefined,
+  error: undefined,
+  loading: false,
+}
+
+const carsReducer = (state = initialState, action: CarsAction): CarsState => {
+  const { type, payload } = action
+  switch (type) {
+    case FETCH_CARS_PENDING:
+      return {
+        data: undefined,
+        error: undefined,
+        loading: true,
+      }
+    case FETCH_CARS_FULFILLED:
+      return {
+        data: payload,
+        error: undefined,
+        loading: false,
+      }
+    case FETCH_CARS_REJECTED:
+      return {
+        data: undefined,
+        error: payload,
+        loading: false,
+      }
+    default:
+      return state
+  }
+}
+
+export default carsReducer
