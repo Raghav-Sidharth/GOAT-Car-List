832e73b66addbdc80fa34dacac24c2e819d0375d
Raghav Sidharth <121782551+Raghav-Sidharth@users.noreply.github.com>
2023-04-07 12:42:09 +1200

    Completed route and db func tested with insomnia

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,18 @@
 import express from 'express'
 import path from 'path'
+import carsRoute from './routes/cars'
+import { resolve } from 'node:path'
 
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
+
+server.use('/api/v1/cars', carsRoute)
+
+// default server.use
+server.get('*', (req, res) => {
+  res.sendFile(resolve('server/public/index.html'))
+})
 
 export default server
